ac9f72d349ffd1cf4fbaa819507743c3f3e67348
obehi234 <obehi.ataga234@gmail.com>
2024-01-25 00:22:06 +0100

    Used circle avater for profile image

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -11,18 +11,19 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(leading: Icon(Icons.bolt_sharp), actions: [
-        ClipOval(
-          child: Container(
-            width: 30,
-            height: 30,
+      appBar: AppBar(
+        leading: Icon(Icons.bolt_sharp),
+        actions: [
+          CircleAvatar(
             child: Image.asset(
               'images/face.png',
               fit: BoxFit.fill,
+              width: 30,
+              height: 30,
             ),
-          ),
-        )
-      ]),
+          )
+        ],
+      ),
     );
   }
 }
