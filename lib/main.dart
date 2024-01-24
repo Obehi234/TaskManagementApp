a06f5b20b7c05871a40088b49b68c6ae917b984d
obehi234 <obehi.ataga234@gmail.com>
2024-01-24 22:32:25 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:task_management_app/task_manager_app.dart';
+
+void main() {
+  runApp(const TaskApp());
+}
+
+
+
+  
---
+++ lib/task_manager_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskApp extends StatefulWidget {
+  const TaskApp({super.key});
+
+  @override
+  State<TaskApp> createState() => _TaskAppState();
+}
+
+class _TaskAppState extends State<TaskApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
